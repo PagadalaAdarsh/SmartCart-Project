83e5aff856bc78c0ce1fe4d541adf4896c76f3e3
PagadalaAdarsh <adarshpagadala6@gmail.com>
2025-03-08 21:55:25 +0530

    Deleted all files from the repository

--- src/main/java/com/project/demo/entity/OrderStatus.java
+++
@@ -1,8 +0,0 @@
-package com.project.demo.entity;
-
-
-public enum OrderStatus {
-	 PENDING,
-	    SUCCESS,
-	    FAILED
-}
--- src/main/java/com/project/demo/entity/Role.java
+++
@@ -1,7 +0,0 @@
-
-package com.project.demo.entity;
-
-public enum Role {
-	ADMIN,
-	CUSTOMER
-}
